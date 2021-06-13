22c4dc7da406a55c8f101f7aec3ed3b627343cb8
anat <anat.dean@verse.co.uk>
2021-06-13 19:17:41 +0100

    fix forced break of url for error testing

--- src/data/api.js
+++ src/data/api.js
@@ -14,7 +14,7 @@ export const fetchConversion = ({ from, to, amount }) => {
   /* I have exposed the api key in here so this project can quickly be ran
    key would be on process.env.REACT_APP_API_KEY, */
   return Axios.get(
-    `https://v6.exchangerate-api.om/v6/5ff10449bb35e2a8da9286b3/pair/${from}/${to}/${amount}`
+    `https://v6.exchangerate-api.com/v6/5ff10449bb35e2a8da9286b3/pair/${from}/${to}/${amount}`
   ).then(({ data }) => {
     console.log(data);
     const {
